0d040b5ea60758c624102776934cbf9506d63cb2
Somanath Shahaji kakade <somanath.kakade5@gmail.com>
2025-03-25 11:51:27 +0530

    added data into localstorage

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const gradientColorBtn = document.querySelector("#bgColorChanger");
 
-
 function updateLinearGradient() {
   // Colors rgb(0,0,0),rgb(0,255,125)
   // red => 0-255,
@@ -14,11 +13,33 @@ function updateLinearGradient() {
   const green2 = Math.floor(Math.random() * 256);
   const blue2 = Math.floor(Math.random() * 256);
 
-  console.log(red1,green1,blue1,red2,green2,blue2);
-  
-  
+  console.log(red1, green1, blue1, red2, green2, blue2);
+
+  localStorage.setItem(
+    "code",
+    JSON.stringify({
+      red1,
+      green1,
+      blue1,
+      red2,
+      green2,
+      blue2,
+    })
+  );
+
   document.body.style.background = `linear-gradient(rgb(${red1},${green1},${blue1}),rgb(${red2},${green2},${blue2}))`;
 }
 
+gradientColorBtn.addEventListener("click", updateLinearGradient);
 
-gradientColorBtn.addEventListener('click', updateLinearGradient);
+window.onload = () => {
+  const retrieveData = localStorage.getItem("code");
+
+  if (retrieveData) {
+    const data = JSON.parse(retrieveData);
+    console.log(data);
+
+    const { red1, green1, blue1, red2, green2, blue2 } = data;
+    document.body.style.background = `linear-gradient(rgb(${red1},${green1},${blue1}),rgb(${red2},${green2},${blue2}))`;
+  }
+};
