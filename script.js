b76dea13477a5634fd63b07de1afbb520fd5227a
Somanath Shahaji kakade <somanath.kakade5@gmail.com>
2025-03-25 11:25:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const gradientColorBtn = document.querySelector("#bgColorChanger");
+
+
+function updateLinearGradient() {
+  // Colors rgb(0,0,0),rgb(0,255,125)
+  // red => 0-255,
+  // green =>0-255,
+
+  const red1 = Math.floor(Math.random() * 256);
+  const green1 = Math.floor(Math.random() * 256);
+  const blue1 = Math.floor(Math.random() * 256);
+
+  const red2 = Math.floor(Math.random() * 256);
+  const green2 = Math.floor(Math.random() * 256);
+  const blue2 = Math.floor(Math.random() * 256);
+
+  console.log(red1,green1,blue1,red2,green2,blue2);
+  
+  
+  document.body.style.background = `linear-gradient(rgb(${red1},${green1},${blue1}),rgb(${red2},${green2},${blue2}))`;
+}
+
+
+gradientColorBtn.addEventListener('click', updateLinearGradient);
